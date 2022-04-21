699ab20b90d636b4b30f827d682571339c4bf078
arora <aroramadhav65@gmail.com>
2022-04-21 20:06:02 +0530

    Update

--- lib/widgets/rounded_button.dart
+++ lib/widgets/rounded_button.dart
@@ -17,7 +17,6 @@ class RoundedButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => press,
       child: Container(
         width: double.infinity,
         alignment: Alignment.center,
