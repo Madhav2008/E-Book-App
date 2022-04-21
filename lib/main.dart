bf962acc4bd26f0aefb3e129c01895dcbdb54d47
arora <aroramadhav65@gmail.com>
2022-04-21 19:45:33 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        
         title: Text(widget.title),
       ),
       body: Center(
