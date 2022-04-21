de42deac24627e0e0446cf25a0210fbc8657ddbe
arora <aroramadhav65@gmail.com>
2022-04-21 20:06:11 +0530

    Update

--- lib/widgets/rounded_button.dart
+++ lib/widgets/rounded_button.dart
@@ -16,30 +16,28 @@ class RoundedButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      child: Container(
-        width: double.infinity,
-        alignment: Alignment.center,
-        margin: EdgeInsets.symmetric(vertical: 16),
-        padding: EdgeInsets.symmetric(
-            vertical: verticalPadding, horizontal: horizontalPadding),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(30),
-          boxShadow: [
-            BoxShadow(
-              offset: Offset(0, 15),
-              blurRadius: 30,
-              color: Color(0xFF666666).withOpacity(.11),
-            ),
-          ],
-        ),
-        child: Text(
-          text,
-          style: TextStyle(
-            fontSize: fontSize,
-            fontWeight: FontWeight.bold,
+    return Container(
+      width: double.infinity,
+      alignment: Alignment.center,
+      margin: EdgeInsets.symmetric(vertical: 16),
+      padding: EdgeInsets.symmetric(
+          vertical: verticalPadding, horizontal: horizontalPadding),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 15),
+            blurRadius: 30,
+            color: Color(0xFF666666).withOpacity(.11),
           ),
+        ],
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: fontSize,
+          fontWeight: FontWeight.bold,
         ),
       ),
     );
