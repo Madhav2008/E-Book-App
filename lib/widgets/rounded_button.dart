be70a1e20d7103e5752490df2cb0644d00a03b80
arora <aroramadhav65@gmail.com>
2022-04-21 19:53:18 +0530

    Update

--- lib/widgets/rounded_button.dart
+++ lib/widgets/rounded_button.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class RoundedButton extends StatelessWidget {
-
   final String text;
   final Function press;
   final double verticalPadding;
@@ -14,19 +13,19 @@ class RoundedButton extends StatelessWidget {
     required this.press,
     this.verticalPadding = 16,
     this.horizontalPadding = 30,
-    this.fontSize = 16, 
+    this.fontSize = 16,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: ()=>press,
+      onTap: () => press,
       child: Container(
         width: double.infinity,
         alignment: Alignment.center,
         margin: EdgeInsets.symmetric(vertical: 16),
-        padding:
-            EdgeInsets.symmetric(vertical: verticalPadding, horizontal: horizontalPadding),
+        padding: EdgeInsets.symmetric(
+            vertical: verticalPadding, horizontal: horizontalPadding),
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(30),
