f1930455f44992c7414ebd8310dd77c823cb2126
arora <aroramadhav65@gmail.com>
2022-04-21 20:08:23 +0530

    Update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -54,7 +54,7 @@ class HomeScreen extends StatelessWidget {
                           auth: "Gary Venchuk",
                           rating: 4.9,
                           pressDetails: () {
-                            Navigator.push(
+                            return Navigator.push(
                               context,
                               MaterialPageRoute(
                                 builder: (context) {
