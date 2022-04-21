5dcfc3876dfe48cb12e3da6d2b2d8124dffcca34
arora <aroramadhav65@gmail.com>
2022-04-21 19:59:40 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:ebook_app/screens/home_screen.dart';
 import 'package:ebook_app/widgets/rounded_button.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp(),);
 
 class MyApp extends StatelessWidget {
   @override
